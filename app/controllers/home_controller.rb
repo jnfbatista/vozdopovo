9ed931fef636c7eb647f3d12bcd4b8613674e66b
Ricardo Gonçalves <ricardo@hardquarters.com>
2011-07-24 15:35:58 +0100

    coise

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,12 @@ class HomeController < ApplicationController
 
   def index
     @ouve = Ouve.new
+
+    @ouves = Ouve.all
+    @nums = [[rand(220), rand(320)],
+             [rand(220), rand(320)],
+             [rand(220), rand(320)],
+             [rand(220), rand(320)]]
   end
 
 end
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,4 +6,25 @@ $(function() {
   $("#submit").click(function() {
     $("#formHidden").slideToggle();
   });
+
+  $(".ouve").hover(function() {
+    $("#map .pos[id=\"" + $(this).attr("id") + "\"]").addClass("selected");
+  }, function() {
+    $("#map .pos[id=\"" + $(this).attr("id") + "\"]").removeClass("selected");
+  });
+
+  $("#back").click(function(e) {
+    e.preventDefault();
+    $("#content .wrapper").css("margin-left", "0");
+    $("#ouves").css("width", "636px");
+    $("#back").css("visibility", "hidden");
+  });
+
+
+  $(".ouve").click(function(e) {
+    e.preventDefault();
+    $("#content .wrapper").css("margin-left", "-260px");
+    $("#ouves").css("width", "400px");
+    $("#back").css("visibility", "visible");
+  });
 });
